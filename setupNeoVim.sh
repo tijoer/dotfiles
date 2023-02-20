773eb7aab2334f758806eb79839e6b927e68e2a0
tijoer <Tim@>
2023-02-20 20:47:00 +0000

    Bugfix

--- setupNeoVim.sh
+++ setupNeoVim.sh
@@ -18,6 +18,9 @@ echo "Install new NodeJs version..."
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&\
 	sudo apt-get install -y nodejs
 
+pip3 install pynvim
+npm i -g neovim
+
 ## Install vim-plug as a plugin manager
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
