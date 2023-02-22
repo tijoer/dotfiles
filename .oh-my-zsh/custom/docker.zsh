8674188bed479cdf31d2871ebbdd175c287a517e
tijoer <tim@>
2023-02-22 10:49:24 +0100

    Bugfix

--- .oh-my-zsh/custom/docker.zsh
+++ .oh-my-zsh/custom/docker.zsh
@@ -1,6 +1,10 @@
 alias dkp='docker ps'
 alias dkpa='docker ps -a'
+
 alias dkk='docker kill'
-alias dke='docker exec -it'
 # kill and delete all
 alias dkka='docker rm $(docker kill $(docker ps -aq))' 
+
+alias dke='docker exec -it'
+
+alias dkcl='docker container ls -a'
