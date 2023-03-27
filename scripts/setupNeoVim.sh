4de4568ad3925ecd56d47159fa63c3f8e4538b64
tijoer <Tim@>
2023-03-27 10:13:39 +0000

    WIP

--- scripts/setupNeoVim.sh
+++ scripts/setupNeoVim.sh
@@ -22,14 +22,10 @@ sudo apt install -y python3-pip
 pip3 install pynvim
 sudo npm i -g neovim
 
-## Install vim-plug as a plugin manager
-#sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-
 #nvim --headless '+PlugInstall --sync' +qa
 #nvim --headless '+CocInstall coc-rust-analyzer --sync' +qa
-#nvim --headless +PackerSync +qall
+#nvim --headless +PackerSync +qall 2&> /dev/null || true
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'silent PackerSync'
 
 # Install win32yank into the bin folder to make neovim integration withing WSL possible.
 sudo apt-get install -y unzip
